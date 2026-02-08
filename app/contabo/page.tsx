bf8362e60a968a965fe6c2ae97ba4c7a2becf6f6
AO AppsOwner SPA <110705397+appsowner@users.noreply.github.com>
2026-02-08 17:28:19 -0300

    Implement redirect to external URL in Contabo page
    Redirects users to an external URL on component mount.

--- app/contabo/page.tsx
+++ app/contabo/page.tsx
@@ -1,1 +1,13 @@
 
+// app/contabo/page.tsx
+"use client";
+
+import { useEffect } from "react";
+
+export default function ContaboRedirectPage() {
+  useEffect(() => {
+    window.location.href = "https://www.kqzyfj.com/ld104p-85-7NPOPRVTXVXNPTOQQRVO?sid=cmp_siteweb";
+  }, []);
+
+  return null;
+}
